92a541f24899284173266ca9f58353989eb1f4f6
Fayozbek <120027251+fayzullayevv@users.noreply.github.com>
2023-03-15 16:27:28 +0500

    Add files via upload

---
+++ while_python.py
@@ -0,0 +1,124 @@
+# 1
+# def new_func(n):
+#     s = 0
+#     while n>s:
+#         a = 'salom olam'
+#         s+=1 
+#         print(a)
+
+# new_func(n = int(input()))
+
+
+# 2
+# def new_func(n):
+#     s = 0
+#     while n>s:
+#         s+=1 
+#         print(s)
+
+# new_func(n = int(input()))
+
+# 3
+# def new_func(n):
+#     s = 0
+#     while n>s:
+#         if n%2==0:
+#             print(0)
+#             s+=1
+#         else:
+#             print(1)
+#             s+=1
+#     s+=1
+
+# new_func(n = int(input()))
+
+
+# 4
+# def new_func(n):
+#     s = 0
+#     while n>s:
+#         if s%2==0:
+#             print(s)
+#             s+=0
+#         s+=1 
+
+# new_func(n = int(input()))
+
+
+# 5
+# def new_func(n):
+#     s = 0
+#     while n>s:
+#         if s%2>0:
+#             print(s)
+#             s+=0
+#         s+=1 
+
+# new_func(n = int(input()))
+
+
+# 6
+# def new_func(n):
+#     s = 0
+#     d = 0
+#     while n>s:
+#         s+=1
+#         d+=s
+#     print(d) 
+#     s+=1
+
+# new_func(n = int(input()))
+
+
+# 7
+# def new_func(n):
+#     s = 0
+#     a = 0 
+#     while n>s:
+#         if s%2>0:
+#             s+=0
+#             a+=s
+#         s+=1
+#     print(a)
+
+# new_func(n = int(input()))
+
+
+# 8
+# def new_func(n):
+#     s = 0
+#     a = 0 
+#     while n>s:
+#         if s%2==0:
+#             s+=0
+#             a+=s
+#         s+=1
+#     print(a)
+
+# new_func(n = int(input()))
+
+
+# 9
+# def sonlar():
+#   s = 1
+#   a = 16
+#   while s<a:
+#       print(s,s*2)
+#       s+=1
+
+# sonlar()
+
+
+# 10
+# def new_func(n):
+#     s = 0
+#     a = 0
+#     while s<n:
+#         if s%10==0:
+#             s+=0
+#             a+=s
+#         s+=1
+#     print(a)
+
+# new_func(n = int(input()))
+
