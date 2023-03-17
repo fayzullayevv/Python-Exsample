07cb50ce87c134dedf88aacbed8c9c83533ab21d
Fayozbek <120027251+fayzullayevv@users.noreply.github.com>
2023-03-17 17:14:04 +0500

    Create 5,1.py

---
+++ 5,1.py
@@ -0,0 +1,60 @@
+# 1
+# def summ(a,b):
+#     return a+b
+
+# print(summ(a=int(input()),b=int(input())))
+
+
+# 2
+# def MaxMin(a,b):
+#     if a>b:
+#         return a
+#     elif a<b:
+#         return b
+    
+# print(MaxMin(a = int(input()),b = int(input())))
+
+
+# 3
+# def MaxMin(a,b,c):
+#     if a>b and a>c:
+#         return a
+#     elif a<b and b>c:
+#         return b
+#     elif c>a and c>b:
+#         return c
+#     else:
+#         return 'Ikkita bir xil son kiritmang!'
+    
+# print(MaxMin(a = int(input()),b = int(input()),c=int(input())))
+
+
+# 4
+# def Modul(x):
+#     return abs(x)
+
+# print(Modul(x=int(input())))
+
+
+# 5
+# def IsEven(a):
+#    return a%2==0
+
+# print(IsEven(a=int(input())))
+
+
+# 6
+# def IsOdd(a):
+#     return a%2==1
+
+# print(IsOdd(a=int(input())))
+
+
+# 7
+# def ConvertMoney(a):
+#     return f"{a/10}$"
+
+# print(ConvertMoney(a=int(input())))
+
+
+# 8
